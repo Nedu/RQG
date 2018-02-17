2fa25ecc3ba06e476e81a38316484113284fe415
Nedu Robert <nedumrobert@gmail.com>
2018-02-17 16:19:28 +0100

    Refactored code to use ES6

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,30 @@
 const newQuote = document.querySelector('.newQuote');
-const base = 'https://andruxnet-random-famous-quotes.p.mashape.com/';
-const myHeaders = new Headers({
-	'Content-Type': 'application/x-www-form-urlencoded',
-	'X-Mashape-Key': 'pULfj53G2cmshTOYMVswroUxQXOFp1XKvibjsnMGH80M08e1Di',
-	Accept: 'application/json'
-})
+const url = 'https://andruxnet-random-famous-quotes.p.mashape.com/';
+const headers = new Headers({
+  'Content-Type': 'application/x-www-form-urlencoded',
+  'X-Mashape-Key': 'pULfj53G2cmshTOYMVswroUxQXOFp1XKvibjsnMGH80M08e1Di',
+  Accept: 'application/json',
+});
 
+// function to get quote from API using fetch
 function getQuote() {
-	fetch(base, {headers: myHeaders})
-		.then(function (response) {
-			return response.json();
-		})
-		.then(function (data) {
-			displayQuote(data.quote, data.author);
-			// displayAuthor(data.author);
-		})
-		.catch(function () {
-			console.log('An error occured');
-		});
+  fetch(url, { headers })
+    .then(response => response.json())
+    .then(data => displayQuote(data.quote, data.author))
+    .catch(err => console.log(`An error: ${err} occured`));
 }
 
+// function to display quote
 function displayQuote(quote, author) {
-	const quoteText = document.querySelector('.quote');
-	const authorText = document.querySelector('#author');
-	const tweetQuote = document.querySelector('.tweet');
-	const a = quote + " - " + author;;
-	
-	quoteText.textContent = quote;
-	authorText.textContent = author;
-	
-	tweetQuote.setAttribute('href', `https://twitter.com/intent/tweet?text=${a}`);
-	// tweetQuote.setAttribute('data-text', `${quote} - ${author}`);
-}
+  const quoteText = document.querySelector('.quote');
+  const authorText = document.querySelector('#author');
+  const tweetQuote = document.querySelector('.tweet');
 
-// function displayAuthor(author) {
-// 	const authorText = document.querySelector('#author');
-// 	authorText.textContent = author;
-// }
+  quoteText.textContent = quote;
+  authorText.textContent = author;
+
+  tweetQuote.setAttribute('href', `https://twitter.com/intent/tweet?text=${quote}-${author}`);
+}
 
 newQuote.addEventListener('click', getQuote);
 getQuote();
